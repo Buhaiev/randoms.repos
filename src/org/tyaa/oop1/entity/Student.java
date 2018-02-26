38fc33ba93b215accc301e796dd57eec8a560d52
student <student@CR541>
2018-02-26 17:21:40 +0200

    group

--- src/org/tyaa/oop1/entity/Student.java
+++ src/org/tyaa/oop1/entity/Student.java
@@ -10,7 +10,48 @@ package org.tyaa.oop1.entity;
  * @author student
  */
 public class Student {
+    public int getGroupId() {
+        return id;
+    }
+    public void setGroupId(int id) throws Exception {
+        
+        if (id >= 0) {
+            
+            this.id = id;
+        } else {
+            
+            throw new Exception("id должен быть больше или равен 0");
+        }
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) throws Exception {
+        
+        if (id >= 0) {
+            
+            this.id = id;
+        } else {
+            
+            throw new Exception("id должен быть больше или равен 0");
+        }
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String name) {
+        this.lastName = name;
+    }
     
+//Позвонить Саше
     public int id;
     public String firstName;
     public String lastName;
